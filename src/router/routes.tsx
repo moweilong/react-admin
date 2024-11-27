c3a9e863abde5f3d9066fd0be21c2911fc63973e
bugsmo <18550039021@163.com>
2024-11-27 22:39:04 +0800

    feat: reactrouter V7 fallbackElement替换为HydrateFallback

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,12 +2,14 @@ import { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import Error404 from '@/components/Error/404';
+import Error500 from '@/components/Error/500';
 import LazyLoadComponent from '@/components/LazyLoadComponent';
 
 import { getRoutes } from './utils';
 
+import type { IRouteObject } from '@/types/custom-types';
+
 import Layout from '@/layouts/index';
-import { IRouteObject } from '@/types/custom-types';
 
 const routeArray = getRoutes();
 
@@ -27,6 +29,7 @@ const routes: IRouteObject[] = [
     meta: {
       menu: false,
     },
+    HydrateFallback: Error500,
     children: [
       {
         index: true,
